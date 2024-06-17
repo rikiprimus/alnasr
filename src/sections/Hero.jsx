8fa857a0c1b5c0caf964ab21d10e2c025aa774c4
rikiprimus <rikiprimus33@gmail.com>
2024-06-18 01:26:52 +0700

    update error

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import Button from '../components/button/button';
+import Button from '../components/button/Button';
 import Navbar from '../components/navbar/navbar';
 import Rectangle from '../assets/photo/rectangle.svg';
 
